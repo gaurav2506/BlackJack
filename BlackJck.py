12ee4ef0c5a3999da599d7e98dbd9a5299ef49f5
Gaurav <gaurav.sukre@outlook.com>
2018-08-10 23:40:09 +0530

    First Commit

---
+++ BlackJck.py
@@ -0,0 +1,44 @@
+import random
+
+dealer_cards= []
+
+player_cards = []
+
+while len(dealer_cards) != 2:
+    dealer_cards.append(random.randint(1,11))
+    if len(dealer_cards)==2:
+        print('Dealer has X &',dealer_cards[1])                                   #DEALER CARDS
+        
+while len(player_cards) != 2:
+    player_cards.append(random.randint(1,11))
+    if len(player_cards)==2:
+        print('You have',player_cards)                                         #PLAYER CARDS
+
+
+if sum(dealer_cards) ==21:
+    print('Dealer has 21 and  wins')
+elif sum(dealer_cards) >21:
+    print ('Dealer has busted')
+
+
+while sum(player_cards) < 21 :
+    action = str(input('Do you want to stay or hit?'))
+    if action == 'hit':
+        player_cards.append(random.randint(1,11))
+        print('You now have a total of ' +str(sum(player_cards))+' from these cards',player_cards)
+    else:
+        print('The dealer has a total of ' + str(sum(dealer_cards))+' with',dealer_cards)
+        print('You have a total of  '+ str(sum(player_cards))+' with',player_cards)
+        if sum(dealer_cards) > sum(player_cards) :
+                print('Dealer Wins!')
+                
+        else:
+                print ('Bhai Party !!!')
+                break
+        
+
+if sum(player_cards) >21:
+    print('You Busted !Dealer Wins')
+elif sum(player_cards) == 21:
+    print('**********************Blackjack!!!!!!!!!!!!!!!!!!!**************')
+
